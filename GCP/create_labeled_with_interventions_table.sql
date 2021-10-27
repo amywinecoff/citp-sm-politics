4a59595a9bdcdbe21999cfbb7bd7443b6700c1a2
amywinecoff <aawinecoff@gmail.com>
2021-10-27 12:24:24 -0500

    updated sql to be clearer, to make control and treatment subreddit tables consistent so hopefully a union can be done

--- GCP/create_labeled_with_interventions_table.sql
+++ GCP/create_labeled_with_interventions_table.sql
@@ -1,4 +1,21 @@
-create table `citp-sm-reactions.reddit_clean_comments.labled_comments_with_interventions` as
+---create a table that only contains subreddits' first two intervention periods. So, if a subreddit had three different
+---periods, only retain the first two
+
+create table `citp-sm-reactions.reddit_clean_comments.reaction_interventions` as (
+with max_ints as (
+    select subreddit
+    FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions_epoc`
+    group by subreddit
+    having max(int_order) > 1)
+SELECT base.*
+FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions_epoc` as base
+INNER JOIN max_ints on max_ints.subreddit = base.subreddit
+WHERE int_order < 3
+ORDER BY subreddit, intervention);
+
+---join the updated interventions table to the interventions table to select only posts that are associated with subreddits
+---that experienced an intervention.
+create table `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct_txt` as
     (
         SELECT distinct(sub.post_id), sub.link_id, sub.sub_id, sub.subreddit, sub.author, byte_length(sub.body) as body_length, sub.nest_level, sub.post_score,
             author_count.author_posts_per_subreddit,
@@ -7,15 +24,13 @@ create table `citp-sm-reactions.reddit_clean_comments.labled_comments_with_inter
             sub.generalized_call, sub.situational_call_for_action,
             sub.collective_rhetoric, sub.ungrounded_argument,
             rxn.intervention, rxn.start_date_unix_epoc as int_start_unix_time, rxn.end_date_unix_epoc as int_end_unix_time, sub.unix_epoc_time as post_unix_time
-        FROM `citp-sm-reactions.reddit_clean_comments.subcategory_labeled` as sub
-        INNER JOIN `citp-sm-reactions.reddit_clean_comments.reaction_interventions_epoc` as rxn
+        FROM `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct` as sub
+        INNER JOIN `citp-sm-reactions.reddit_clean_comments.reaction_interventions` as rxn
             ON (sub.unix_epoc_time >= rxn.start_date_unix_epoc) AND (sub.unix_epoc_time <= rxn.end_date_unix_epoc)
             AND sub.subreddit = rxn.subreddit
         LEFT JOIN (select author, subreddit,COUNT(author) as author_posts_per_subreddit
-                FROM `citp-sm-reactions.reddit_clean_comments.subcategory_labeled`
+                FROM `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct`
                 WHERE BYTE_LENGTH(author) > 0
                 group by author, subreddit) as author_count on author_count.author = sub.author AND author_count.subreddit = sub.subreddit
     );
---     create table citp-sm-reactions.reddit_clean_comments.reaction_interventions_epoc_new as (
--- SELECT TRIM(subreddit) AS subreddit, intervention, start_date, end_date, start_date_unix_epoc, end_date_unix_epoc
--- FROM citp-sm-reactions.reddit_clean_comments.reaction_interventions_epoc);
+
--- GCP/select_control_subreddits.sql
+++ GCP/select_control_subreddits.sql
@@ -1,16 +1,14 @@
---create a table that contains observations for each potential control subreddit for each upvote treatment subreddit by aligning
---observations within the same period as the treatment was applied for each subreddit that had the baseline-->upvote only treatment
- create table `citp-sm-reactions.reddit_clean_comments.ctr_subreddits_for_upvote` as (
+ create table `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_ctr` as (
  with cart as (--create the Cartesian product between upvote treatment subreddits and all other subreddits
      SELECT  distinct(ctr.subreddit) as ctr_subreddit, txt.subreddit
-     FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions_txt` as txt, `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct` as ctr
-     WHERE txt.subreddit != ctr.subreddit AND ctr.subreddit not in (select distinct(subreddit) from `citp-sm-reactions.reddit_clean_comments.reaction_interventions_txt`)
+     FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions` as txt, `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct` as ctr
+     WHERE txt.subreddit != ctr.subreddit AND ctr.subreddit not in (select distinct(subreddit) from `citp-sm-reactions.reddit_clean_comments.reaction_interventions`)
      order by subreddit, ctr_subreddit
  )
- SELECT dt.*, cart.ctr_subreddit, ctr.post_id, ctr.link_id, ctr.sub_id, ctr.unix_epoc_time, ctr.who_instead_of_what, ctr.we_vs_them, ctr.fact_related_argument, ctr.structured_argument,
+ SELECT dt.*, cart.ctr_subreddit, ctr.post_id, ctr.link_id, ctr.sub_id, ctr.author, ctr.unix_epoc_time, ctr.who_instead_of_what, ctr.we_vs_them, ctr.fact_related_argument, ctr.structured_argument,
              ctr.counter_argument_structure, ctr.emotional_language,ctr.other, ctr.you_in_the_epicenter, ctr.empathy_reciprocity,
              ctr.generalized_call, ctr.situational_call_for_action,
-             ctr.collective_rhetoric, ctr.ungrounded_argument, ctr.nest_level, byte_length(ctr.body) as body_length,
+             ctr.collective_rhetoric, ctr.ungrounded_argument, ctr.nest_level, byte_length(ctr.body) as body_length, author_count.author_posts_per_subreddit,
      CASE
          WHEN ctr.unix_epoc_time >= baseline_start AND ctr.unix_epoc_time <= baseline_end THEN 'baseline'
          WHEN ctr.unix_epoc_time >= upvote_start AND ctr.unix_epoc_time <= upvote_end THEN 'upvote_only'
@@ -21,19 +19,23 @@
          min(CASE WHEN intervention = 'baseline' THEN end_date_unix_epoc END) AS baseline_end,
          min(CASE WHEN intervention = 'upvote_only' THEN start_date_unix_epoc END) AS upvote_start,
          min(CASE WHEN intervention = 'upvote_only' THEN end_date_unix_epoc END) AS upvote_end
-         FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions_txt`
-         where subreddit not in ('vegan', 'unpopularopinion')--these subreddits did not have the upvote intervention
+         FROM `citp-sm-reactions.reddit_clean_comments.reaction_interventions`
+         where subreddit not in ('vegan', 'unpopularopinion', 'EnoughTrumpSpam')--vegan and unpopularopinion subreddits did not have the upvote intervention. EnoughTrumpSpam only has 13 observations in the baseline period
          group by subreddit) as dt
  LEFT JOIN cart on cart.subreddit = dt.txt_subreddit
  LEFT JOIN `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct` as ctr
      ON ctr.subreddit = cart.ctr_subreddit
+ LEFT JOIN (select author, subreddit,COUNT(author) as author_posts_per_subreddit
+                FROM `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct`
+                WHERE BYTE_LENGTH(author) > 0
+                group by author, subreddit) as author_count on author_count.author = ctr.author AND author_count.subreddit = ctr.subreddit
  );
 
 --create a table that contains the average values of the language subcomponents and the total number of observations per period to enable
 --matching to the treatment subreddits. Note that the aggregates are also computed for the upvote period so that subreddits can also
 --be matched on number of observations during the intervention, but the average language subcomponent values SHOULD NOT be used for
 --selection of the appropriate controls
- create table `citp-sm-reactions.reddit_clean_comments.ctr_subreddits_for_upvote_avg` as (
+ create table `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_ctr` as (
  SELECT txt_subreddit, ctr_subreddit, period,
      avg(counter_argument_structure) as ctr_avg_counter_argument_structure,
      avg(emotional_language) as ctr_avg_emotional_language,
@@ -45,13 +47,13 @@
      avg(collective_rhetoric) as ctr_avg_collective_rhetoric,
      avg(ungrounded_argument) as ctr_avg_ungrounded_argument,
      count(post_id) as ctr_obs_per_period,
- from `citp-sm-reactions.reddit_clean_comments.ctr_subreddits_for_upvote`
+ from `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_ctr`
  group by ctr_subreddit, period, txt_subreddit
  HAVING period != 'no_period'
  order by txt_subreddit, ctr_subreddit, period);
 
 --create a table that contains the same info for the upvote treatment subreddits for subsequent matching to the control subreddits
-create table `citp-sm-reactions.reddit_clean_comments.txt_subreddits_for_upvote_avg` as (
+create table `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_txt` as (
 SELECT subreddit as txt_subreddit, intervention as period,
     avg(counter_argument_structure) as txt_avg_counter_argument_structure,
     avg(emotional_language) as txt_avg_emotional_language,
@@ -63,14 +65,15 @@ SELECT subreddit as txt_subreddit, intervention as period,
     avg(collective_rhetoric) as txt_avg_collective_rhetoric,
     avg(ungrounded_argument) as txt_avg_ungrounded_argument,
     count(post_id) as txt_obs_per_period,
-from `citp-sm-reactions.reddit_clean_comments.labeled_comments_with_interventions_txt`
+from `citp-sm-reactions.reddit_clean_comments.subcategory_labeled_distinct_txt`
+WHERE subreddit != 'EnoughTrumpSpam'
 group by intervention, subreddit
 HAVING intervention != 'no_votes'
-order by subreddit, intervention);
+order by subreddit, intervention);;
 
 -- create a table joining the treatment average language values and control average language values to allow correlations to be calculated elsewhere
 -- too hard to figure out how to do rowise correlations within SQL
-create table `citp-sm-reactions.reddit_clean_comments.txt_subreddits_for_upvote_txt_and_ctr_avg` as (
+create table `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_txt_and_ctr_avg` as (
 SELECT txt.txt_subreddit,
       ctr.ctr_subreddit,
       txt.txt_obs_per_period as txt_obs_per_baseline,
@@ -93,14 +96,14 @@ SELECT txt.txt_subreddit,
       ctr.ctr_avg_situational_call_for_action,
       ctr.ctr_avg_collective_rhetoric,
       ctr.ctr_avg_ungrounded_argument,
-FROM `citp-sm-reactions.reddit_clean_comments.txt_subreddits_for_upvote_avg` txt
-JOIN `citp-sm-reactions.reddit_clean_comments.ctr_subreddits_for_upvote_avg` ctr
+FROM `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_txt` txt
+JOIN `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_ctr` ctr
    ON ctr.txt_subreddit = txt.txt_subreddit and ctr.period = txt.period
 JOIN (SELECT txt_subreddit, txt_obs_per_period as txt_obs_per_upvote
-       from `citp-sm-reactions.reddit_clean_comments.txt_subreddits_for_upvote_txt_avg`
+       from `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_txt`
        WHERE period = 'upvote_only') txt_obs on txt_obs.txt_subreddit = txt.txt_subreddit
 JOIN (SELECT ctr_subreddit, txt_subreddit, ctr_obs_per_period as ctr_obs_per_upvote
-       from `citp-sm-reactions.reddit_clean_comments.ctr_subreddits_for_upvote_avg`
+       from `citp-sm-reactions.reddit_clean_comments.subreddits_for_upvote_avg_ctr`
        WHERE period = 'upvote_only') ctr_obs on ctr_obs.ctr_subreddit = ctr.ctr_subreddit
            and ctr_obs.txt_subreddit = ctr.txt_subreddit
 WHERE txt.period = 'baseline'
